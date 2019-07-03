44d4ee715d790ee6ca47b9241f671c3c4fdf5fdc
Valentine <32904482+vlav98@users.noreply.github.com>
2019-07-03 17:50:26 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,72 +2,110 @@ $(document).ready(function () {
     const observer = lozad();
     observer.observe();
 
-    $(window).on('resize', function () {
-        if (window.matchMedia("(min-width: 767.98px)").matches) {
-            $(function () {
-                $.scrollify({
-                    section: ".section",
-                    interstitialSection: "footer",
-                    easing: "easeOutExpo",
-                    scrollSpeed: 1100,
-                    offset: 0,
-                    scrollbars: true,
-                    standardScrollElements: "",
-                    setHeights: true,
-                    overflowScroll: true,
-                    updateHash: true,
-                    touchScroll: true,
-                    before: function () { },
-                    after: function () { },
-                    afterResize: function () { },
-                    afterRender: function () { }
-                });
-            });
-            
-            $.fn.isInViewport = function () {
-                var elementTop = $(this).offset().top;
-                var elementBottom = elementTop + $(this).outerHeight();
+    $(function () {
+        $.scrollify({
+            section: ".section",
+            interstitialSection: "footer",
+            easing: "easeOutExpo",
+            scrollSpeed: 1100,
+            offset: 0,
+            scrollbars: true,
+            standardScrollElements: "",
+            setHeights: true,
+            overflowScroll: true,
+            updateHash: true,
+            touchScroll: true,
+            before: function () {},
+            after: function () {},
+            afterResize: function () {},
+            afterRender: function () {}
+        });
+    });
 
-                var viewportTop = $(window).scrollTop();
-                var viewportBottom = viewportTop + $(window).height();
-
-                return elementBottom > viewportTop && elementTop < viewportBottom;
-            };
-
-            $(window).on('resize scroll', function () {
-                $('section').each(function () {
-                    var activeSlide = $(this).attr('id');
-                    if ($('#home').isInViewport()) {
-                        $('.pagination').css("display", "none");
-                    } else if ($(this).isInViewport() && $(this) !== $('#home')) {
-                        $('.pagination').css("display", "block");
-                        if (activeSlide == $('#qnav-' + activeSlide).attr('id').slice(5)) {
-                            $('#qnav-' + activeSlide).addClass('active');
-                        }
-                    }
-                    else {
-                        $('#qnav-' + activeSlide).removeClass('active');
-                    }
-                });
-            });
-
+    if (window.matchMedia("(max-width: 991.98px)").matches) {
+        $.scrollify.disable();
+    }
+    $(window).resize(function () {
+        if (window.matchMedia("(max-width: 991.98px)").matches) {
+            $.scrollify.disable();
         } else {
-            /* L'affichage est inférieur à 600px de large */
+            $.scrollify.enable();
         }
+    });
+    var box = $('.box');
+    var button = $('.open-menu, .header-menu');
+    button.on('click', function () {
+        box.toggleClass('active');
+    });
+    $(".access-btn").click(function () {
+        $(".access-panel").animate({
+            width: "toggle"
+        });
     });
 
     $('.js-scrollTo').on('click', function () { // Au clic sur un élément
         var page = $(this).attr('href'); // Page cible
         var speed = 750; // Durée de l'animation (en ms)
-        $('html, body').animate({ scrollTop: $(page).offset().top }, speed); // Go
+        $('html, body').animate({
+            scrollTop: $(page).offset().top
+        }, speed); // Go
+        return false;
+    });
+    $('#discover-content').on('click', function () { // Au clic sur un élément
+        var page = "#sekou"; // Page cible
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate({
+            scrollTop: $(page).offset().top
+        }, speed); // Go
         return false;
     });
 
-    $(".podcast-btn").each(function(index) {
-        $(".podcast-btn").click(function () {
-            $("scApi").find(index).toggle();
-        });
+    $('#footer-btn').on('click', function () { // Au clic sur un élément
+       /*  if ($('#footerResp').length) {
+            var page = "#footerResp"; // Page cible
+            var speed = 750; // Durée de l'animation (en ms)
+            $('html, body').animate({
+                scrollTop: $(page).offset().top
+            }, speed); // Go
+            return false;
+        } */
+        $('#footerResp').toggle();
+    }); 
+
+    $(".podcast-btn").click(function () {
+        if ($('.scApi').css("visibility") === "hidden") {
+            $(".scApi").css("visibility", "visible");
+        } else {
+            $(".scApi").css("visibility", "hidden");
+        }
     });
+
+    if ($('.pagination').length) {
+        $.fn.isInViewport = function () {
+            var elementTop = $(this).offset().top;
+            var elementBottom = elementTop + $(this).outerHeight();
+
+            var viewportTop = $(window).scrollTop();
+            var viewportBottom = viewportTop + $(window).height();
+
+            return elementBottom > viewportTop && elementTop < viewportBottom;
+        };
+        $(window).on('resize scroll', function () {
+            $('section').each(function () {
+                var activeSlide = $(this).attr('id');
+                if ($('#home').isInViewport()) {
+                    $('.pagination').css("display", "none");
+                } else if ($(this).isInViewport() && $(this) !== $('#home')) {
+                    $('.pagination').css("display", "block");
+                    if (activeSlide == $('#qnav-' + activeSlide).attr('id').slice(5)) {
+                        $('#qnav-' + activeSlide).addClass('active');
+                    }
+                } else {
+                    $('#qnav-' + activeSlide).removeClass('active');
+                }
+            });
+        });
+    }
 });
 
 var prevScrollpos = window.pageYOffset;
